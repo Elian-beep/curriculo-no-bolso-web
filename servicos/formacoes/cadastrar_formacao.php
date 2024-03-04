ff879c570e40eb385f241101333e8527e2537e4b
AlecSiluca <alecsander.silva.da.costa@gmail.com>
2024-03-03 23:49:13 -0400

    [TELAS/SERVIÇOS] criação do CADASTRAR_FORMACAO e de sua tela

---
+++ servicos/formacoes/cadastrar_formacao.php
@@ -0,0 +1,18 @@
+<?php
+include_once("../config.php");
+if(isset($_POST["cadastrar"])){
+    $instituicao = $_POST["instituicao"];
+    $curso = $_POST["curso"];
+    $inicio = $_POST["inicio"];
+    $termino = $_POST["termino"];
+    $id_tipo =$_POST["id_tipo"];
+    $id_usuario = $_POST["id_usuario"];
+
+    $sql = "INSERT INTO formacoes (instituicao, curso, inicio ,termino, id_tipo, id_usuario) VALUES ('$instituicao', '$curso', '$inicio' , '$termino', '$id_tipo', '$id_usuario');";
+    $result = $conexao->query($sql);
+    header("Location: ");
+}else{
+    header("Location: ");
+}
+
+?>
