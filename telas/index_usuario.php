6e195dce4bbad98e386eb74899b12df9cd43bf9f
Elian-beep <elian.19batista@gmail.com>
2024-03-04 11:05:42 -0300

    [TELAS] index de usuario

---
+++ telas/index_usuario.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Usuários</title>
+</head>
+<body>
+    <h1>Usuários</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>NOME COMPLETO</th>
+                <th>E-MAIL</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            ?>
+        </tbody>
+    </table>
+</body>
+</html>
